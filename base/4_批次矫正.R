45e0af7f3f5f6396c7526c5ea46660f1e96ede43
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-01-19 20:49:38 +0800

    Update 4_批次矫正.R

--- base/4_批次矫正.R
+++ base/4_批次矫正.R
@@ -19,5 +19,5 @@ ggsave(plot = p, filename =paste0(dir, "/batch_correction_after_umap.pdf"), widt
 seurat <- FindNeighbors(object = seurat, reduction = "integrated.cca", dims = 1:50)
 seurat <- FindClusters(seurat, resolution = seq(0.1, 1, by = 0.1))
 p <- DimPlot(seurat, group.by = paste0("integrated.cca_snn_res.", seq(0.1, 1, by = 0.1)), dims = 1:50, ncol = 3)
-ggsave(plot = p, filename =paste0(dir, "/batch_correction_after_umap.pdf"), width = 20, height = 20, units = "cm") # 保存批次矫正后的umap图
+ggsave(plot = p, filename = paste0(dir, "/batch_correction_after_umap.pdf"), width = 20, height = 20, units = "cm") # 保存批次矫正后的umap图
 qs_save(seurat, paste0(dir, "batch_correction.qs2"))
