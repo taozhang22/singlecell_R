f65236db3e63b048b91058a6042eaa7cc9324558
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-01-19 15:54:38 +0800

    Update 质量控制.R

--- 读取文件/质量控制.R
+++ 读取文件/质量控制.R
@@ -1,7 +1,5 @@
-# 质量控制图
+# 质量控制图--------------------------------------------------------------------
 root <- sqrt(length(unique(seurat$Sample)))
-dir <- "figure"
-dir.create(dir, showWarnings = F)
 
 # 绘制nCount_RNA与percent.mt的关系图
 seurat[["percent.mt"]] <- PercentageFeatureSet(seurat, pattern = "^MT-")
@@ -13,7 +11,7 @@ for (sample in unique(seurat$Sample)) {
 }
 p <- wrap_plots(p_list, nrow = round(root))
 p
-ggsave(plot = p, filename = "figure/nCount_mt.pdf", height = 5 * size, width = 4 * size)
+ggsave(plot = p, filename = "nCount_mt.pdf", height = 5 * root, width = 4 * root)
 
 # 绘制nCount_RNA与nFeature_RNA的关系图
 p_list <- list()
@@ -24,8 +22,15 @@ for (sample in unique(seurat$Sample)) {
 }
 p <- wrap_plots(p_list, nrow = round(root))
 p
-ggsave(plot = p, filename = "nCount_nFeature.pdf", height = 4 * size, width = 4 * size)
+ggsave(plot = p, filename = "nCount_nFeature.pdf", height = 4 * root, width = 4 * root)
 
 # 绘制质量控制的小提琴图("nCount_RNA", "nFeature_RNA", "percent.mt")
 p <- VlnPlot(object = seurat, features = c("nCount_RNA", "nFeature_RNA", "percent.mt"), layer = "counts", ncol = 1); p
-ggsave(plot = p, filename = paste0(dir, "figure/nCount_nFeature_mt.pdf"), width = 0.6 * length(unique(seurat$Sample)), height = 20)
+ggsave(plot = p, filename = "nCount_nFeature_mt.pdf", width = 0.6 * length(unique(seurat$Sample)), height = 20)
+
+# 查看每个样本的细胞的数量
+# 1) 统计并转成数据框
+tab <- table(seurat$Sample)
+df  <- data.frame(Sample = names(tab), CellCount = as.integer(tab), row.names = NULL)
+ggplot(df, aes(x = reorder(Sample, CellCount), y = CellCount)) +
+  geom_col()
