d4ce8770f0cdb8a9d0bc845ce70b0e28fc3570fc
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-01-19 20:18:31 +0800

    Update 4_批次矫正.R

--- base/4_批次矫正.R
+++ base/4_批次矫正.R
@@ -4,10 +4,11 @@ seurat <- FindVariableFeatures(seurat, nfeatures = 3000)
 seurat <- ScaleData(seurat)
 seurat <- RunPCA(seurat)
 p <- ElbowPlot(object = seurat, ndims = 50); p
-ggsave(plot = p, filename =paste0(dir, "/去除批次效应_肘状图.pdf"), width = 20, height = 20, units = "cm")
+ggsave(plot = p, filename =paste0(dir, "/batch_correction_elbowplot.pdf"), width = 20, height = 20, units = "cm")
 seurat <- IntegrateLayers(object = seurat, method = CCAIntegration, orig.reduction = "pca", new.reduction = "integrated.cca", verbose = T)
 
 seurat[["RNA"]] <- JoinLayers(seurat[["RNA"]])
 seurat <- FindNeighbors(object = seurat, reduction = "integrated.cca", dims = 1:50)
 seurat <- FindClusters(seurat, resolution = seq(0.1, 1, by = 0.1))
 seurat <- RunUMAP(seurat, group.by = paste0("integrated_snn_res.", seq(0.1, 1, by = 0.1)), dims = 1:50, reduction = "integrated.cca")
+qs_save(seurat, paste0(dir, "batch_correction.qs2"))
