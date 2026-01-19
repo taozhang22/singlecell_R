5d090cac7dee50f2e6abe320d3c8d99971dbd613
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-01-19 11:20:32 +0800

    Update GSE144735.R

--- 读取文件/GSE144735.R
+++ 读取文件/GSE144735.R
@@ -3,19 +3,24 @@ library(data.table)
 library(tidyverse)
 library(Seurat)
 library(qs2)
-setwd("D:/research/bioinformatics/singlecell/dataset/GSE144735")
+library(patchwork)
+setwd("D:/research/bioinformatics/singlecell/database/GSE144735")
 
-# 读取表达矩阵
-seurat <- fread("GSE144735_processed_KUL3_CRC_10X_raw_UMI_count_matrix.txt.gz") %>% 
+filename_seurat <- "GSE144735_processed_KUL3_CRC_10X_raw_UMI_count_matrix.txt.gz"
+filename_metadata <- "GSE144735_processed_KUL3_CRC_10X_annotation.txt.gz"
+filename_out <- "GSE144735"
+
+# 创建seurat对象
+seurat <- fread(filename_seurat) %>% 
   column_to_rownames(var = "Index") 
 
-# 读取metadata数据
-metadata <- fread("GSE144735_processed_KUL3_CRC_10X_annotation.txt.gz") %>% 
+metadata <- fread(filename_metadata) %>% 
   mutate(Sample = word(Index, 1, sep = "_")) %>% 
-  left_join(fread("GSE144735.txt"), by = "Sample") %>% 
+  left_join(fread(paste0(filename_out, ".txt")), by = "Sample") %>% 
   column_to_rownames(var = "Index") %>% 
   transmute(Sample, MSI, Class)
-metadata <- metadata[colnames(seurat), ] # 让metadata里面的行名与表达矩阵的列名完全的一致（包括顺序）
+metadata <- metadata[colnames(seurat), ]
+table(rownames(metadata) == names(seurat)) # 查看seurat的列名和metadata的行名是不是完全一致，包括顺序
 
-seurat <- CreateSeuratObject(counts = seurat, project = "GSE144735", min.cells = 3, min.features = 100, meta.data = metadata) # 创建seurat对象
-qs_save(seurat, "GSE144735.qs2")
+seurat <- CreateSeuratObject(counts = seurat, project = "filename_out", min.cells = 3, min.features = 100, meta.data = metadata)
+qs_save(seurat, paste0(filename_out, ".qs2"))
