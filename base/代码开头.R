d2e37c27d473f44e19af4069aa0cc9ba3edeb166
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-01-19 12:03:21 +0800

    Create 代码开头.R

---
+++ base/代码开头.R
@@ -0,0 +1,7 @@
+rm(list = ls()); gc()
+library(data.table)
+library(tidyverse)
+library(qs2)
+library(Seurat)
+library(patchwork)
+setwd("D:/research/bioinformatics/singlecell/R/practice/") #根据情况修改工作路径
