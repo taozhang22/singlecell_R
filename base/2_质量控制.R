c6348d9a6295f13a5bf8cdc9c9f5f437f483f07f
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-01-19 12:05:26 +0800

    Create 2_质量控制.R

---
+++ base/2_质量控制.R
@@ -0,0 +1,9 @@
+seurat[["percent.mt"]] <- PercentageFeatureSet(seurat, pattern = "^MT-")
+p1 <- FeatureScatter(seurat, feature1 = "nCount_RNA", feature2 = "percent.mt")
+p2 <- FeatureScatter(seurat, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+p <- p1 + p2; p
+ggsave(plot = p, filename = paste0(dir, "/质量控制_nCount_mt_nFeature.pdf"), width = 60, height = 20, units = "cm")
+
+p <- VlnPlot(object = seurat, features = c("nCount_RNA", "nFeature_RNA", "percent.mt"), layer = "counts", ncol = 1); p
+ggsave(plot = p, filename = paste0(dir, "/质量控制_nCount_nFeature_mt.pdf"), width = 0.6 * length(unique(seurat$Sample)), height = 20)
+seu.all <- subset(seu.all, subset = percent.mt < 20) # 根据情况修改质量控制的条件
