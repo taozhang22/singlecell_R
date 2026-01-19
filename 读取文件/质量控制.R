78acf00187f3f48e14849a7dbc0c369ab14af6b8
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-01-19 16:57:18 +0800

    Update 质量控制.R

--- 读取文件/质量控制.R
+++ 读取文件/质量控制.R
@@ -30,7 +30,7 @@ ggsave(plot = p, filename = "nCount_nFeature_mt.pdf", width = 0.6 * length(uniqu
 
 # 查看每个样本的细胞的数量
 # 1) 统计并转成数据框
-tab <- table(seurat$Sample)
-df  <- data.frame(Sample = names(tab), CellCount = as.integer(tab), row.names = NULL)
+df <- table(seurat$Sample)
+df <- data.frame(Sample = names(tab), CellCount = as.integer(tab), row.names = NULL)
 ggplot(df, aes(x = reorder(Sample, CellCount), y = CellCount)) +
   geom_col()
