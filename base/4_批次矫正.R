97f6fd93d133c060a310119a096f94dad36f6f8c
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-01-19 20:26:29 +0800

    Update 4_批次矫正.R

--- base/4_批次矫正.R
+++ base/4_批次矫正.R
@@ -5,10 +5,13 @@ seurat <- ScaleData(seurat)
 seurat <- RunPCA(seurat)
 p <- ElbowPlot(object = seurat, ndims = 50); p
 ggsave(plot = p, filename =paste0(dir, "/batch_correction_elbowplot.pdf"), width = 20, height = 20, units = "cm")
+DimPlot(seurat, group.by = "Sample")
+ggsave(plot = p, filename =paste0(dir, "/batch_correction_before_elbowplot.pdf"), width = 20, height = 20, units = "cm")
+
 seurat <- IntegrateLayers(object = seurat, method = CCAIntegration, orig.reduction = "pca", new.reduction = "integrated.cca", verbose = T)
 
 seurat[["RNA"]] <- JoinLayers(seurat[["RNA"]])
+seurat <- RunUMAP(seurat, group.by = paste0("integrated_snn_res.", seq(0.1, 1, by = 0.1)), dims = 1:50, reduction = "integrated.cca")
 seurat <- FindNeighbors(object = seurat, reduction = "integrated.cca", dims = 1:50)
 seurat <- FindClusters(seurat, resolution = seq(0.1, 1, by = 0.1))
-seurat <- RunUMAP(seurat, group.by = paste0("integrated_snn_res.", seq(0.1, 1, by = 0.1)), dims = 1:50, reduction = "integrated.cca")
 qs_save(seurat, paste0(dir, "batch_correction.qs2"))
