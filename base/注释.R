6fb05fe8556e9558d4712d6fe84a56c7b9734190
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-01-19 20:22:50 +0800

    Create 注释.R

---
+++ base/注释.R
@@ -0,0 +1,16 @@
+# 注释--------------------------------------------------------------------------
+classical.markers <- c(
+  "CD79A", "MS4A1", "BANK1",                     # B cell
+  "PLVAP", "VWF",                                # Endothelial
+  "PLP1", "CLU", "S100B",                        # Enteric glial
+  "KRT18", "KRT8", "EPCAM",                      # Epithelial
+  "COL1A1", "DCN", "COL3A1",                     # Fibroblast
+  "MS4A2", "KIT", "TPSAB1",                      # MAST
+  "LYZ", "CD68", "CD14",                         # McDC
+  "G0S2", "FCGR3B", "CSF3R",                     # Neutrophil
+  "LILRA4", "IL3RA", "CLEC4C",                   # pDC
+  "MCAM", "PDGFRB", "CSPG4",                     # Pericyte
+  "MZB1", "DERL3", "IGHG2",                      # Plasma B
+  "CNN1", "TAGLN", "DES",                        # Smooth muscle cell
+  "CD3D", "CD3E", "KLRB1"                        # T_NK
+)
