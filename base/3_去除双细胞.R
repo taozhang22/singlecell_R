5128fc474677b6418799b62ce8fac471cfa1c0a5
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-01-19 20:11:28 +0800

    Create 3_去除双细胞.R

---
+++ base/3_去除双细胞.R
@@ -0,0 +1,31 @@
+# 去除双细胞--------------------------------------------------------------------
+seurat <- lapply((seurat), function(seu) {
+  seu <- NormalizeData(seu)
+  seu <- FindVariableFeatures(seu, nfeatures = 3000)
+  seu <- ScaleData(seu)
+  seu <- RunPCA(seu)
+  seu <- RunUMAP(seu, dims = 1:50)
+  seu <- FindNeighbors(seu, dims = 1:50)
+  seu <- FindClusters(seu, resolution = 0.5)
+  
+  sweep <- paramSweep(seu, PCs = 1:50, sct = FALSE)
+  sweep <- summarizeSweep(sweep, GT = FALSE)
+  pk <- find.pK(sweep)
+  pk <- as.numeric(as.character(pk$pK[which.max(pk$BCmetric)]))
+  
+  annotations <- seu@meta.data$seurat_clusters
+  homotypic.prop <- modelHomotypic(annotations) # 估算同型双细胞的比例
+  nExp_poi <- round(ncol(seu)/1000 * 0.008 * nrow(seu@meta.data)) # 算出双细胞的数量
+  nExp_poi.adj <- round(nExp_poi * (1 - homotypic.prop)) # 去除同型双细胞的数量
+  seu <- doubletFinder(seu, PCs = 1:50, pN = 0.25, pK = pk, nExp = nExp_poi.adj, reuse.pANN = NULL, sct = FALSE)
+  
+  seu$doublet.class <- seu[[paste0("DF.classifications_0.25_", pk, "_", nExp_poi.adj)]]
+  seu[[paste0("DF.classifications_0.25_", pk, "_", nExp_poi.adj)]] <- NULL
+  pann <- grep("pANN_", names(seu@meta.data), value = TRUE)
+  seu$pANN <- seu[[pann]]; seu[[pann]] <- NULL
+  
+  return(seu)
+})
+seurat <- merge(seurat[[1]], seurat[-1])
+seurat <- subset(seurat, doublet.class == "Singlet")
+seurat <- DietSeurat(seurat, assays = "RNA", layers = "counts") 
