a00a5a0cbc2d3c5603e80ad99b8435ee74de3371
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-01-21 09:10:55 +0800

    Update 0_代码开头.R

--- base/0_代码开头.R
+++ base/0_代码开头.R
@@ -1,4 +1,17 @@
+# Rstudio版本
 rm(list = ls()); gc()
+library(data.table)
+library(tidyverse)
+library(qs2)
+library(Seurat)
+library(patchwork)
+setwd("D:/research/bioinformatics/singlecell/R/practice/") #根据情况修改工作路径
+
+dir <- "result/base" # 根据情况修改文件夹的名字
+dir.create(dir, showWarnings = FALSE, recursive = TRUE)
+
+# vscode
+rm(list = ls())
 library(data.table)
 library(tidyverse)
 library(qs2)
