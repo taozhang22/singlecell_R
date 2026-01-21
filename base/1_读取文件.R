c820a0d03799e4e143a6324086d44d66f3cf77ca
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-01-21 15:23:21 +0800

    Update 1_读取文件.R

--- base/1_读取文件.R
+++ base/1_读取文件.R
@@ -1,5 +1,5 @@
 # 读取文件----------------------------------------------------------------------
-ids <- c("GSE132465", "GSE144735", "GSE144735")
+ids <- c("GSE132465")
 seurat <- lapply(ids, function(id) {
   seu <- qs_read(paste0("../../database/", id, "/", id, ".qs2"))
   
