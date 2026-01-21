2c4ab882d381a9f75938a0e9b2dc4bd9ca63680c
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-01-21 09:26:01 +0800

    Update 1_读取文件.R

--- base/1_读取文件.R
+++ base/1_读取文件.R
@@ -1,4 +1,8 @@
 # 读取文件----------------------------------------------------------------------
-seurat1 <- qs_read("../../database/GSE144735/GSE144735.qs2")
-seurat2 <- qs_read("../../database/GSE144735/GSE144735.qs2")
-seurat
+ids <- c("GSE132465", "GSE144735", "GSE144735")
+seurat <- lapply(ids, function(id) {
+  seu <- qs_read(paste0("../../database/", id, "/", id, ".qs2"))
+  
+  return(seu)
+})
+seurat <- merge(seurat[[1]], seurat[-1])
