5b4f393c2d7916ce357a45d93e406a8584c29977
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-01-21 09:20:13 +0800

    Update 1_读取文件.R

--- base/1_读取文件.R
+++ base/1_读取文件.R
@@ -1,2 +1,4 @@
 # 读取文件----------------------------------------------------------------------
-seurat <- qs_read("../../database/GSE144735/GSE144735.qs2")
+seurat1 <- qs_read("../../database/GSE144735/GSE144735.qs2")
+seurat2 <- qs_read("../../database/GSE144735/GSE144735.qs2")
+seurat
