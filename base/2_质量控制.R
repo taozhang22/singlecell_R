5b4980e570f831340771c767e669dda79246747d
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-01-21 09:26:30 +0800

    Add quality control plots and subset conditions

--- base/2_质量控制.R
+++ base/2_质量控制.R
@@ -1,4 +1,5 @@
 # 质量控制----------------------------------------------------------------------
+
 seurat[["percent.mt"]] <- PercentageFeatureSet(seurat, pattern = "^MT-")
 p1 <- FeatureScatter(seurat, feature1 = "nCount_RNA", feature2 = "percent.mt")
 p2 <- FeatureScatter(seurat, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
@@ -9,3 +10,4 @@ p <- VlnPlot(object = seurat, features = c("nCount_RNA", "nFeature_RNA", "percen
 ggsave(plot = p, filename = paste0(dir, "/qc_nCount_nFeature_mt.pdf"), width = 0.6 * length(unique(seurat$Sample)), height = 20)
 seurat <- subset(seurat, subset = percent.mt < 20) # 根据情况修改质量控制的条件
 qs_save(seurat, paste0(dir, "qc.qs2"))
+
